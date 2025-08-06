dffd9f5113bbc01c6620f886cd554829004ecf2b
harshith2709481 <gharshith1818@gmail.com>
2025-08-06 07:51:38 +0530

    Added navbar in feature/navbar branch

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,15 @@ function App() {
 
   return (
     <>
+      {/* Navigation Bar */}
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+        </ul>
+      </nav>
+
+      {/* Logo Section */}
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,7 +25,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
       <h1>Vite + React</h1>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -25,6 +36,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
